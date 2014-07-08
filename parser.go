81b6fb003a1ae07a5136305ba3c989a88aae0176
Alif Rachmawadi <subosito@gmail.com>
2014-07-08 09:25:56 +0700

    check snowcrash version to ensure minimum required version

--- parser.go
+++ parser.go
@@ -3,9 +3,12 @@ package iglo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	version "github.com/hashicorp/go-version"
 	"io"
 	"io/ioutil"
 	"os/exec"
+	"strings"
 )
 
 func ParseJSON(r io.Reader) (*API, error) {
@@ -24,9 +27,14 @@ func ParseJSON(r io.Reader) (*API, error) {
 }
 
 func ParseMarkdown(r io.Reader) ([]byte, error) {
-	path, err := exec.LookPath("snowcrash")
+	path, err := snowcrash()
 	if err != nil {
-		return nil, errors.New("Couldn't find snowcrash. Please install it first https://github.com/apiaryio/snowcrash")
+		return nil, err
+	}
+
+	err = detectVersion()
+	if err != nil {
+		return nil, err
 	}
 
 	b, err := ioutil.ReadAll(r)
@@ -46,4 +54,56 @@ func ParseMarkdown(r io.Reader) ([]byte, error) {
 	cmd.Stdin = out
 
 	return cmd.Output()
+}
+
+func CheckVersion(v string) error {
+	mv, _ := version.NewVersion("0.9.0")
+	ov, err := version.NewVersion(v)
+	if err != nil {
+		return err
+	}
+
+	if ov.LessThan(mv) {
+		return errors.New(fmt.Sprintf("You are using snowcrash version %s. Minimum version should be %s", ov, mv))
+	}
+
+	return nil
+}
+
+func detectVersion() error {
+	v, err := snowcrashVersion()
+	if err != nil {
+		return err
+	}
+
+	err = CheckVersion(v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func snowcrash() (string, error) {
+	path, err := exec.LookPath("snowcrash")
+	if err != nil {
+		return "", errors.New("Couldn't find snowcrash. Please install it first https://github.com/apiaryio/snowcrash")
+	}
+
+	return path, nil
+}
+
+func snowcrashVersion() (string, error) {
+	path, err := snowcrash()
+	if err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command(path, "--version")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(strings.Replace(string(output), "v", "", 1)), nil
 }
--- parser_test.go
+++ parser_test.go
@@ -24,3 +24,30 @@ func TestParse(t *testing.T) {
 		}
 	}
 }
+
+func TestCheckVersion(t *testing.T) {
+	versions := map[string]bool{
+		"0.12.0": true,
+		"0.11.0": true,
+		"0.10.1": true,
+		"0.10.0": true,
+		"0.9.0":  true,
+		"0.8.1":  false,
+		"0.8.0":  false,
+		"0.7.5":  false,
+		"0.7.4":  false,
+		"0.7.3":  false,
+		"0.7.2":  false,
+		"0.7.1":  false,
+		"0.7.0":  false,
+		"0.1.1":  false,
+	}
+
+	for v, b := range versions {
+		err := CheckVersion(v)
+
+		if (err != nil) == b {
+			t.Errorf("CheckVersion() is returning %s with version %s", err, v)
+		}
+	}
+}
