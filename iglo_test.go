436e3a8e5b31aba3a1f120678bd0854ae88471b4
Ryan Hatfield <ryan@ryan.bio>
2015-12-03 20:01:29 -0600

    Add basic test to watch for drafter version changes

---
+++ iglo_test.go
@@ -0,0 +1,28 @@
+package iglo
+
+import (
+	"os/exec"
+	"reflect"
+	"testing"
+)
+
+func TestDrafterVersion(t *testing.T) {
+	path, err := drafter()
+	if err != nil {
+		t.Error("Drafter is not available on the system.")
+		panic(err)
+	}
+
+	cmd := exec.Command(path, "--version")
+
+	output, err := cmd.Output()
+	if err != nil {
+		t.Error("The drafter command failed.")
+		panic(err)
+	}
+
+	expected := []byte("v2.0.1\n")
+	if !reflect.DeepEqual(output, expected) {
+		t.Errorf("Expected:\n%+v\nActual:\n%+v", string(expected), string(output))
+	}
+}
